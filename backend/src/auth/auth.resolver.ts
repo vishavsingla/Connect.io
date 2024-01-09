8b101342749b2eab6cd4b37546bede0f54cfe9b8
VishavSingla <vishav9933@gmail.com>
2024-01-10 03:41:34 +0530

    backend auth started
    backend auth started

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,4 +1,10 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+
+    @Query(() => String)
+    async hello(){
+        return 'hello';
+    }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import e, { Request, Response } from 'express';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly JwtService: JwtService,
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService,
+
+    ){}
+
+    async refreshToken(req: Request, res: Response){
+        const refreshToken = req.cookies['refreshToken'];
+
+        if(!refreshToken){
+            throw new UnauthorizedException('Refresh token not found');
+        }
+
+        let payload;
+
+        try{
+            payload = this.JwtService.verify(refreshToken,{
+                secret: this.configService.get<string>('REFRESH_TOKEN_SECRET')
+            });
+
+        }catch(error){
+            throw new UnauthorizedException('Invalid or expired refresh token')
+        }
+
+        const userExists = await this.prisma.user.findUnique({
+            where:{id: payload.sub},
+        })
+
+        if(!userExists){
+            throw new BadRequestException('User no longer exists');
+        }
+
+        const expiresIn = 15000;
+
+        const expiration = Math.floor(Date.now()/1000) + expiresIn;
+
+        const accessToken = this.JwtService.sign(
+            {...payload, exp: expiration},
+            {
+                secret: this.configService.get<string>('ACCESS_TOKEN_SECRET')
+            },
+        );
+
+        res.cookie('access_token', accessToken, {httpOnly: true});
+
+        return accessToken;
+    }
+}
