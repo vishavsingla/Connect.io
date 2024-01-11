b45925c3926df9111272f28a0bbce2ce5abb6791
VishavSingla <vishav9933@gmail.com>
2024-01-11 21:17:42 +0530

    auth controller completed user started
    auth controller completed user started

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Context, Mutation } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './user.type';
+import { Request } from 'express';
+import { UseGuards } from '@nestjs/common';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import {v4 as uuid} from 'uuid';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js'
+
+
 
 @Resolver()
 export class UserResolver {}
